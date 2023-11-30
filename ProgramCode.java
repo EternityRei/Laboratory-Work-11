94a957ed0aac24ae1032e6be6e82986efc788e20
Anastasia Mayorova <90724127+EternityRei@users.noreply.github.com>
2023-11-30 13:05:38 +0200

    Update ProgramCode.java

--- ProgramCode.java
+++ ProgramCode.java
@@ -1,59 +1,105 @@
+/**
+ * Represents a plan pattern for managing microclimates.
+ * It includes details such as associated microclimate, device information, and plan parameters.
+ */
+@Entity
+@Table(name = "PlanPattern")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 @Builder
-@Table(name = "PlanPattern")
 public class PlanPattern {
 
+    /**
+     * Unique identifier for the PlanPattern.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    /**
+     * The optimal microclimate associated with this plan pattern.
+     */
     @OneToOne
     @JoinColumn(name = "optimalmicroclimate_id", referencedColumnName = "id")
     private Microclimate microclimate;
 
+    /**
+     * A list of microclimate plans linked to this plan pattern.
+     */
     @OneToMany(mappedBy = "planPattern")
     private List<MicroclimatePlan> microclimatePlans;
 
+    /**
+     * Information about the device associated with this plan pattern.
+     */
     @Column
     private String device;
 
+    /**
+     * Parameters for the plan associated with this plan pattern.
+     */
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "planParameters_id", referencedColumnName = "id")
     private PlanParameters planParameters;
 }
 
-
+/**
+ * Data Transfer Object for PlanPattern.
+ * It's used to transfer data between processes, ensuring that only the necessary data is communicated.
+ */
 @Data
 @Schema(description = "Plan Pattern Data Transfer Object")
 public class PlanPatternDTO {
 
+    /**
+     * Identifier for the PlanPattern.
+     */
     @Schema(description = "PlanPattern id")
     private Integer id;
 
+    /**
+     * Device information for the PlanPattern. It must be null when creating a new PlanPattern.
+     */
     @Null(groups = OnCreate.class, message = "Devices must be null at fulling the form")
     @Schema(description = "Plan devices")
     private String device;
 
+    /**
+     * Microclimate details for the PlanPattern. It should be null when creating a new PlanPattern for the first time.
+     */
     @Null(groups = OnCreate.class, message = "Microclimate should be null while 1st time creating")
     @Valid
     @Schema(description = "Microclimate")
     private MicroclimateDTO microclimateDTO;
 
+    /**
+     * Plan parameters for the PlanPattern. It cannot be null when creating a new PlanPattern.
+     */
     @NotNull(groups = OnCreate.class, message = "Plan parameters cannot be null")
     @Valid
     @Schema(description = "Plan Parameters")
     private PlanParametersDTO planParametersDTO;
 }
 
+/**
+ * Repository interface for PlanPattern entities.
+ * It provides methods to perform operations such as finding a plan pattern by its ID.
+ */
 @Repository
 public interface PlanPatternRepository extends JpaRepository<PlanPattern, Integer> {
+    /**
+     * Find a PlanPattern by its identifier.
+     *
+     * @param id The identifier of the PlanPattern.
+     * @return An Optional containing the PlanPattern if found.
+     */
     Optional<PlanPattern> findPlanPatternById(Integer id);
 }
 
-
+/**
+ * Service interface defining operations for managing PlanPatterns.
+ */
 public interface PlanPatternService {
     PlanPatternDTO createPlanPattern(PlanPatternDTO planPatternDTO);
     PlanPatternDTO updatePlanPattern(Integer id, PlanPatternDTO planPatternDTO);
@@ -62,29 +108,43 @@ public interface PlanPatternService {
     List<PlanPatternDTO> getAllPlanPatterns();
 }
 
+/**
+ * Implementation of PlanPatternService.
+ * Provides functionality for managing PlanPatterns, including creation, update, deletion, and retrieval.
+ */
 @Service
 @RequiredArgsConstructor
 public class PlanPatternServiceImpl implements PlanPatternService{
 
     private final PlanPatternRepository planPatternRepository;
     private final PlanPatternMapper planPatternMapper;
-
+    
+    /**
+     * Creates a new PlanPattern entity from a DTO and saves it to the repository.
+     *
+     * @param planPatternDTO The PlanPatternDTO containing the data to be saved.
+     * @return The saved PlanPatternDTO with generated ID and persisted data.
+     */
     @Override
     public PlanPatternDTO createPlanPattern(PlanPatternDTO planPatternDTO) {
         PlanPattern planPattern = planPatternMapper.toPlanPattern(planPatternDTO);
         return planPatternMapper.toPlanPatternDTO(planPatternRepository.save(planPattern));
     }
 
+    /**
+     * Updates an existing PlanPattern entity based on a provided DTO.
+     *
+     * @param id The ID of the PlanPattern to be updated.
+     * @param planPatternDTO The PlanPatternDTO containing updated data.
+     * @return The updated PlanPatternDTO.
+     * @throws EntityNotFoundException If no PlanPattern is found with the provided ID.
+     */
     @Override
     public PlanPatternDTO updatePlanPattern(Integer id, PlanPatternDTO planPatternDTO) {
-
-        // Fetch the existing plan pattern
         PlanPattern planPattern = planPatternRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(StatusCodes.ENTITY_NOT_FOUND.name(), "Plan pattern not found"));
 
         planPatternValidation(planPatternDTO, OnUpdate.class);
-
-        // If everything is fine, update the plan pattern
         PlanPattern newPlanPattern = planPatternMapper.toPlanPattern(planPatternDTO);
         planPattern.setMicroclimatePlans(newPlanPattern.getMicroclimatePlans());
         planPattern.setDevice(newPlanPattern.getDevice());
@@ -93,6 +153,12 @@ public class PlanPatternServiceImpl implements PlanPatternService{
         return planPatternMapper.toPlanPatternDTO(planPatternRepository.save(planPattern));
     }
 
+    /**
+     * Deletes a PlanPattern entity from the repository based on the given ID.
+     *
+     * @param id The ID of the PlanPattern to be deleted.
+     * @throws EntityNotFoundException If no PlanPattern is found with the provided ID.
+     */
     @Override
     public void deletePlanPattern(Integer id) {
         PlanPattern planPattern = planPatternRepository.findById(id)
@@ -101,6 +167,13 @@ public class PlanPatternServiceImpl implements PlanPatternService{
         planPatternRepository.deleteById(planPattern.getId());
     }
 
+    /**
+     * Retrieves a PlanPatternDTO by its ID.
+     *
+     * @param id The ID of the PlanPattern to be retrieved.
+     * @return The retrieved PlanPatternDTO.
+     * @throws EntityNotFoundException If no PlanPattern is found with the provided ID.
+     */
     @Override
     public PlanPatternDTO getPlanPatternById(Integer id) {
         PlanPattern planPattern = planPatternRepository.findById(id)
@@ -109,12 +182,24 @@ public class PlanPatternServiceImpl implements PlanPatternService{
         return planPatternMapper.toPlanPatternDTO(planPattern);
     }
 
+    /**
+     * Retrieves all PlanPattern entities from the repository.
+     *
+     * @return A list of PlanPatternDTOs.
+     */
     @Override
     public List<PlanPatternDTO> getAllPlanPatterns() {
         List<PlanPattern> planPatterns = planPatternRepository.findAll();
         return planPatternMapper.toPlanPatternDTOS(planPatterns);
     }
 
+    /**
+     * Validates the provided PlanPatternDTO based on the specified validation group.
+     *
+     * @param planPatternDTO The PlanPatternDTO to be validated.
+     * @param validationGroup The validation group class (e.g., OnCreate, OnUpdate).
+     * @throws InvalidDataException If validation fails.
+     */
     private void planPatternValidation(PlanPatternDTO planPatternDTO, Class<?> validationGroup) {
         MicroclimateDTO microclimate = planPatternDTO.getMicroclimateDTO();
         HumidityDTO humidity = microclimate.getHumidity();
