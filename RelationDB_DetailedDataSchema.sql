2ea86980e78c6d74fc7ddd29895a73a9f53719f8
Anastasia Mayorova <90724127+EternityRei@users.noreply.github.com>
2023-11-30 12:03:47 +0200

    Update RelationDB_DetailedDataSchema.sql

--- RelationDB_DetailedDataSchema.sql
+++ RelationDB_DetailedDataSchema.sql
@@ -1,115 +1,105 @@
--- create user table
-    create table "User" (
-    id serial primary key,
-    name varchar(359) check (name
-    	~'^[a-za-z0-9]+$'),
-    surname varchar(350) check (surname
-    	~'^[a-za-z0-9]+$'),
-    email varchar(255) unique check (email   
-        ~'^[a-za-z0-9._%+-]+@[a-za-z0-9.-]+\.[a-za-z]{2,}$'),
-    password varchar(255) check (password  
-        ~'^(?=.*[a-z])(?=.*[a-z])(?=.*\d)(?=.*[\$!?\])[a-za-z\d$!?]{8,}$')
+-- Create user table
+CREATE TABLE "User" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(359) CHECK (name ~'^[a-zA-Z0-9]+$'),
+    surname VARCHAR(350) CHECK (surname ~'^[a-zA-Z0-9]+$'),
+    email VARCHAR(255) UNIQUE CHECK (email ~'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+    password VARCHAR(255) CHECK (password ~'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\$!?\])[a-zA-Z\d$!?]{8,}$')
 );
 
--- create microclimate table
-    create table microclimate (
-    id serial primary key,
-    temperature varchar(20),
-    ventilation varchar(150),
-    lightlevel numeric check (lightlevel > 0),
-    humidity_id int
+-- Create microclimate table
+CREATE TABLE microclimate (
+    id SERIAL PRIMARY KEY,
+    temperature VARCHAR(20),
+    ventilation VARCHAR(150),
+    lightlevel NUMERIC CHECK (lightlevel > 0),
+    humidity_id INT
 );
 
--- create humidity table
-    create table humidity (
-    id serial primary key,
-    relativehumidity numeric check (relativehumidity > 0),
-    absolutehumidity numeric check (absolutehumidity > 0)
+-- Create humidity table
+CREATE TABLE humidity (
+    id SERIAL PRIMARY KEY,
+    relativehumidity NUMERIC CHECK (relativehumidity > 0),
+    absolutehumidity NUMERIC CHECK (absolutehumidity > 0)
 );
 
--- create planparameters table
-    create table planparameters (
-    id serial primary key,
-    temperaturesked varchar(100),
-    lightsofftime timestamp
+-- Create planparameters table
+CREATE TABLE planparameters (
+    id SERIAL PRIMARY KEY,
+    temperaturesked VARCHAR(100),
+    lightsofftime TIMESTAMP
 );
 
--- create planpattern table
-    create table planpattern (
-    id serial primary key,
-    optimalmicroclimate_id int,
-    device varchar(200),
-    planparameters_id int
+-- Create planpattern table
+CREATE TABLE planpattern (
+    id SERIAL PRIMARY KEY,
+    optimalmicroclimate_id INT,
+    device VARCHAR(200),
+    planparameters_id INT
 );
 
--- create theme table
-    create table theme (
-    id int primary key,
-    title varchar(255)
+-- Create theme table
+CREATE TABLE theme (
+    id INT PRIMARY KEY,
+    title VARCHAR(255)
 );
 
--- create topicsinfo table
-    create table topicsinfo (
-    id int primary key,
-    description text,
-    type varchar(255),
-    info bytea
+-- Create topicsinfo table
+CREATE TABLE topicsinfo (
+    id INT PRIMARY KEY,
+    description TEXT,
+    type VARCHAR(255),
+    info BYTEA
 );
 
--- create a junction table for the many-to-many relationship
-    create table themetopicsinfo (
-    theme_id int,
-    topics_info_id int,
-    primary key (theme_id, topics_info_id),
-    constraint fk_theme
-        foreign key (theme_id) 
-        references theme(id),
-    constraint fk_topics_info
-        foreign key (topics_info_id) 
-        references topicsinfo(id)
+-- Create a junction table for the many-to-many relationship
+CREATE TABLE themetopicsinfo (
+    theme_id INT,
+    topics_info_id INT,
+    PRIMARY KEY (theme_id, topics_info_id),
+    CONSTRAINT fk_theme
+        FOREIGN KEY (theme_id) 
+        REFERENCES theme(id),
+    CONSTRAINT fk_topics_info
+        FOREIGN KEY (topics_info_id) 
+        REFERENCES topicsinfo(id)
 );
 
--- create microclimateplan table
-    create table microclimateplan (
-    id serial primary key,
-    planpattern_id int,
-    initiallymicroclimate_id int,
-    user_id int,
-	topic_id int
+-- Create microclimateplan table
+CREATE TABLE microclimateplan (
+    id SERIAL PRIMARY KEY,
+    planpattern_id INT,
+    initiallymicroclimate_id INT,
+    user_id INT,
+    topic_id INT
 );
 
+-- Add foreign key constraints
+ALTER TABLE microclimate
+    ADD CONSTRAINT fk_humidity_id
+        FOREIGN KEY (humidity_id) REFERENCES humidity(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- add foreign key constraints
-    alter table microclimate
-    add constraint fk_humidity_id
-foreign key (humidity_id) references humidity(id) on delete cascade on update
-        cascade;
+ALTER TABLE planpattern
+    ADD CONSTRAINT fk_optimalmicroclimate_id
+        FOREIGN KEY (optimalmicroclimate_id) REFERENCES microclimate(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-    alter table planpattern
-    add constraint fk_optimalmicroclimate_id
-foreign key (optimalmicroclimate_id) references microclimate(id) on delete
-        cascade on update cascade;
+ALTER TABLE planpattern
+    ADD CONSTRAINT fk_planparameters_id
+        FOREIGN KEY (planparameters_id) REFERENCES planparameters(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-    alter table planpattern
-    add constraint fk_planparameters_id
-foreign key (planparameters_id) references planparameters(id) on delete cascade
-        on update cascade;
+ALTER TABLE microclimateplan
+    ADD CONSTRAINT fk_planpattern_id
+        FOREIGN KEY (planpattern_id) REFERENCES planpattern(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-    alter table microclimateplan
-    add constraint fk_planpattern_id
-foreign key (planpattern_id) references planpattern(id) on delete cascade on
-        update cascade;
+ALTER TABLE microclimateplan
+    ADD CONSTRAINT fk_initiallymicroclimate_id
+        FOREIGN KEY (initiallymicroclimate_id) REFERENCES microclimate(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-    alter table microclimateplan
-    add constraint fk_initiallymicroclimate_id
-foreign key (initiallymicroclimate_id) references microclimate(id) on delete
-        cascade on update cascade;
+ALTER TABLE microclimateplan
+    ADD CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id) REFERENCES "User"(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-    alter table microclimateplan
-    add constraint fk_user_id
-foreign key (user_id) references "User"(id) ON DELETE CASCADE ON UPDATE CASCADE;
-
-    alter table microclimateplan
-    add constraint fk_topic_id
-foreign key (topic_id) 
-references topicsinfo(id) on delete cascade on update cascade;
+ALTER TABLE microclimateplan
+    ADD CONSTRAINT fk_topic_id
+        FOREIGN KEY (topic_id) 
+        REFERENCES topicsinfo(id) ON DELETE CASCADE ON UPDATE CASCADE;
